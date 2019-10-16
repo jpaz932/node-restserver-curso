d144dae71a84ba0dc154b4fca5432dec61403dc2
jpaz93 <juanpa.aristis@gmail.com>
2019-10-16 12:32:23 -0500

    var eentorno

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/cafe';
 else
-    urlDB = 'mongodb+srv://jpaz93:3yYZGsUs8JeKAcBg@cluster0-lbp6t.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 
 
 process.env.NODE_URLDB = urlDB;
